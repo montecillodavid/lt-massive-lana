9501b2f45c8a0a49cb26c0b679081130a5a741ff
David Montecillo <montecillodavid@gmail.com>
2015-02-08 05:34:07 +0800

    update error

--- controller.php
+++ controller.php
@@ -2,7 +2,7 @@
 
 	if(isset($_SERVER['PLATFORM'])){
 		if($_SERVER['PLATFORM'] == 'pagoda'){
-			define('DBINFO',  "mysql:host=$_SERVER['DATABASE1_HOST'];:$_SERVER['DATABASE1_PORT'];;dbname=$_SERVER['DATABASE1_NAME'];");
+			define('DBINFO',  "mysql:host=$_SERVER['DATABASE1_HOST'];:$_SERVER['DATABASE1_PORT'];dbname=$_SERVER['DATABASE1_NAME'];");
 			define('DBUSER',  $_SERVER['DATABASE1_USER']);
 			define('DBPASS',  $_SERVER['DATABASE1_PASS']);	
 		}else{

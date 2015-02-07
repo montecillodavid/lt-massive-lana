41b2a132c4be3c23cfa32f797cae0c8579678c2c
David Montecillo <montecillodavid@gmail.com>
2015-02-08 05:43:16 +0800

    fix

--- controller.php
+++ controller.php
@@ -1,15 +1,20 @@
 <?php
 
 	if(isset($_SERVER['PLATFORM'])){
+
 		if($_SERVER['PLATFORM'] == 'pagoda'){
-			define('DBINFO',  "mysql:host=$_SERVER['DATABASE1_HOST'];:$_SERVER['DATABASE1_PORT'];dbname=$_SERVER['DATABASE1_NAME'];");
+			define('DBINFO',  'mysql:host=' . $_SERVER['DATABASE1_HOST'] . ':' . $_SERVER['DATABASE1_PORT'] . ';' . 'dbname=' . $_SERVER['DATABASE1_NAME'] . ';' );
 			define('DBUSER',  $_SERVER['DATABASE1_USER']);
 			define('DBPASS',  $_SERVER['DATABASE1_PASS']);	
 		}else{
 
+			define('DBINFO',  'mysql:/host=localhost;dbname=lockoutfinal;');
+			define('DBUSER',  'root');
+			define('DBPASS',  '');	
 		} 
 
 	}else{
+
 		define('DBINFO',  'mysql:/host=localhost;dbname=lockoutfinal;');
 		define('DBUSER',  'root');
 		define('DBPASS',  '');	 

30a7c2af7d160a181bd6a8f34c414358e3755b81
David Montecillo <montecillodavid@gmail.com>
2015-02-08 04:23:05 +0800

    test

--- controller.php
+++ controller.php
@@ -2,7 +2,7 @@
 
 	if(isset($_SERVER['PLATFORM'])){
 		if($_SERVER['PLATFORM'] == 'pagoda'){
-			define('DBINFO',  'mysql:/host=tunnel.pagodabox.com:3306;dbname=gopagoda;');
+			define('DBINFO',  'mysql:host=tunnel.pagodabox.com:3306;dbname=gopagoda;');
 			define('DBUSER',  $_SERVER['DATABASE1_USER']);
 			define('DBPASS',  $_SERVER['DATABASE1_PASS']);	
 		}else{
